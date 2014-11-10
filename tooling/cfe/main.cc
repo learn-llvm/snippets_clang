a0027cdef9b3e9eb1e8324060083b442a2916331
HongxuChen <leftcopy.chx@gmail.com>
2014-11-10 15:54:56 +0800

    cfe & tokens

---
+++ tooling/cfe/main.cc
@@ -0,0 +1,55 @@
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Host.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/DiagnosticOptions.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/LangOptions.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Parse/Parser.h"
+#include "clang/Parse/ParseAST.h"
+#include <iostream>
+using namespace llvm;
+using namespace clang;
+static cl::opt<std::string> FileName(cl::Positional, cl::desc("Input file"),
+                                     cl::Required);
+int main(int argc, char **argv) {
+  cl::ParseCommandLineOptions(argc, argv, "My simple front end\n");
+  CompilerInstance CI;
+  DiagnosticOptions diagnosticOptions;
+  CI.createDiagnostics();
+  IntrusiveRefCntPtr<TargetOptions> PTO(new TargetOptions());
+  PTO->Triple = sys::getDefaultTargetTriple();
+  TargetInfo *PTI =
+      TargetInfo::CreateTargetInfo(CI.getDiagnostics(), PTO.getPtr());
+  CI.setTarget(PTI);
+
+  CI.createFileManager();
+  CI.createSourceManager(CI.getFileManager());
+  CI.createPreprocessor();
+  CI.getPreprocessorOpts().UsePredefines = false;
+  ASTConsumer *astConsumer = CreateASTPrinter(NULL, "");
+  CI.setASTConsumer(astConsumer);
+  CI.createASTContext();
+  CI.createSema(TU_Complete, NULL);
+  const FileEntry *pFile = CI.getFileManager().getFile(FileName);
+  if (!pFile) {
+    std::cerr << "File not found: " << FileName << std::endl;
+    return 1;
+  }
+  CI.getSourceManager().createMainFileID(pFile);
+  CI.getDiagnosticsClient().BeginSourceFile(CI.getLangOpts(), 0);
+  ParseAST(CI.getSema());
+  // Print AST statistics
+  CI.getASTContext().PrintStats();
+  CI.getASTContext().Idents.PrintStats();
+  return 0;
+}
