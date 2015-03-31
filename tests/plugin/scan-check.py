b36283356200d89541719b1a4d88ebdcdcbfffde
HongxuChen <leftcopy.chx@gmail.com>
2015-03-31 10:37:12 +0800

    correct scan-check.py

--- tests/plugin/scan-check.py
+++ tests/plugin/scan-check.py
@@ -30,6 +30,12 @@ def which(program):
     return None
 
 
+def preexec_func():
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    # os.setpgrp()
+
+
 def cons(prefix, l):
     il = ['{} {}'.format(prefix, i) for i in l]
     return ' '.join(il)
@@ -37,7 +43,7 @@ def cons(prefix, l):
 
 def pp(hint, content):
     print(
-        '{}{:20} {}{}{}'.format(
+        '{}{:10} {}{}{}'.format(
             Fore.YELLOW,
             hint,
             Fore.GREEN,
@@ -60,23 +66,42 @@ def get_plugin_list():
 disabled_list = ['core.DivideZero']
 enabled_list = ['chx.DZChecker', 'chx.NetChecker']
 load_list = get_plugin_list()
+
+
 def cli_parser(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', nargs='+', default=load_list, help='plugins')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '-d',
+        '-P',
+        '--plugin',
+        metavar='PLUGIN',
+        nargs='+',
+        default=load_list,
+        help='plugin')
+    parser.add_argument(
+        '-D',
+        '--disable',
+        metavar='DISABLED',
         nargs='+',
         default=disabled_list,
         help='disabled checkers')
     parser.add_argument(
-        '-e',
+        '-E',
+        '--enable',
+        metavar='ENABLED',
         nargs='+',
         default=enabled_list,
         help='enabled checkers')
     parser.add_argument(
         'options',
+        metavar='COMMAND',
         nargs='+',
         help='regular command line options')
+    parser.add_argument(
+        '-V',
+        '--view',
+        action='store_true',
+        help='view bug reports (if any) in the browser')
     args = parser.parse_args(args)
     return args
 
@@ -93,13 +118,14 @@ def main():
         exe_dict['scan-build'],
         exe_dict['clang'])
     cmd_str = "{} {} {} {}".format(analyzer,
-                                   cons('-load-plugin', args.p),
-                                   cons('-disable-checker', args.d),
-                                   cons('-enable-checker', args.e))
-    for k in [args.p, args.d, args.e]:
-        pp('hhh', k)
+                                   cons('-load-plugin', args.plugin),
+                                   cons('-disable-checker', args.disable),
+                                   cons('-enable-checker', args.enable))
+    args_dict = args.__dict__
+    for k in ['plugin', 'disable', 'enable']:
+        pp(k, args_dict[k])
 
-    for argv in sys.argv[1:]:
+    for argv in args.options:
         cmd_str += (" " + argv)
     print(cmd_str)
 
@@ -107,6 +133,7 @@ def main():
         cmd_str.split(),
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
+    process.wait()
     out, err = process.communicate()
     for line in out.splitlines():
         if exe_dict['ccc-analyzer'] in line:
@@ -116,13 +143,13 @@ def main():
     if NO_BUG in out:
         print(NO_BUG)
         sys.exit(0)
-    view = False
-    if view:
+    if args.view:
         pat = r"Run '(scan-view /tmp/scan-build-.*)' to examine bug reports"
         match = re.search(pat, out)
         assert(match)
         cmd = match.group(1)
-        subprocess.call(cmd.split())
+        proc = subprocess.Popen(cmd.split(), preexec_fn=preexec_func)
+        # proc.wait()
     else:
         print(err)
         print('=' * 80)
